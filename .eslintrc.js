e38542748279a99aed22d04e071191647d0ddb28
David Benson <dmbenson1978@gmail.com>
2018-12-12 11:52:41 +0000

    eslint.rc tweak
    Allows:
    ```const wrapper = shallow(
      <App
        action={() => {}}
      />
    );```

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,13 @@
 module.exports = {
     "extends": "airbnb",
     "rules": {
-        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }]
+        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+        "comma-dangle": ["error", {
+            "arrays": "always-multiline",
+            "objects": "always-multiline",
+            "imports": "always-multiline",
+            "exports": "always-multiline",
+            "functions": "ignore"
+        }]
     },
 };
